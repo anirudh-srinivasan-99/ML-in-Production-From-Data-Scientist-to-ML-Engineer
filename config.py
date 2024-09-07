4d14a1ac7006114d4ba6c4b8c09fd8da488173de
anirudh-srinivasan-99 <anirudhbadri99@gmail.com>
2024-09-07 12:32:11 +0530

    enhancement: sets up loguru

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import DirectoryPath, FilePath
+from loguru import logger
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file = '.env', env_file_encoding='utf-8')
@@ -9,3 +10,8 @@ class Settings(BaseSettings):
     model_name: str
 
 settings = Settings()
+
+logger.add(
+    "app.log", retention='1 week', rotation = '1 day',
+    level = 'INFO'
+)
